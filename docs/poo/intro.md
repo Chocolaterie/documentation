d7b30901a13a5e2fb9efe3377dbc8ccbda1e5347
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-11-21 13:19:50 +0100

    edit main setting and add synthese + tp moyenne

---
+++ docs/poo/intro.md
@@ -0,0 +1,9 @@
+---
+sidebar_position: 1
+---
+
+# Intro
+
+La programmation orientée objet est un paradigme trés important dans le domaine de développement logiciel voir même obligatoire.
+
+Bien qu'il est sympa de connaître de le concept, la pratique est une étape obligatoire pour la maîtrise de ce dernier (beaucoup de pratique).
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,13 +46,6 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -66,7 +59,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Programming',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -76,13 +69,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            label: 'Documentation',
           },
         ],
       },
@@ -93,38 +80,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Documentation',
                 to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
               },
             ],
           },
